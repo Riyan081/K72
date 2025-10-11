9258b867de48b86365ca64e2aa949de901aa7b66
Riyan <waterdlaw810@gmail.com>
2025-10-11 17:06:59 +0530

    done with navbar animation

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,20 @@ import Home from "./pages/Home";
 import Agence from "./pages/Agence";
 import Projects from "./pages/Projects";
 import { Link } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import FullScreenNav from "./components/navbar/FullScreenNav";
 
 function App() {
 
   return (
   <div className="text-white">
-     <Navbar/>
-      <Routes>
+     {/* <Navbar/> */}
+     <FullScreenNav/>
+      {/* <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/agence" element={<Agence />} />
         <Route path="/projects" element={<Projects />} />
-      </Routes>
+      </Routes> */}
     </div>
   );
 }
